d5dfbdd7ae1b95e818e874329b1ab72e5cd57bb2
Abdullah Abdelrazik <abdallah.abdoooo@gmail.com>
2015-12-05 03:33:48 +0200

    Add solution to course 1 module 2 assignment 3

--- 2nd milestone Assignments/ruby-on-rails-intro/module-2/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
+++ 2nd milestone Assignments/ruby-on-rails-intro/module-2/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
@@ -1,14 +1,17 @@
 class Person
   #have a first_name and last_name attribute with public accessors
-  #attr_accessor 
-
+  attr_accessor :first_name,:last_name
+  
   #have a class attribute called `people` that holds an array of objects
   @@people = []
 
   #have an `initialize` method to initialize each instance
-  def initialize(x,y)#should take 2 parameters for first_name and last_name
+  def initialize(first_name,last_name)#should take 2 parameters for first_name and last_name
     #assign those parameters to instance variables
+    @first_name = first_name
+    @last_name = last_name
     #add the created instance (self) to people class variable
+    @@people.push(self)
   end
 
   #have a `search` method to locate all people with a matching `last_name`
@@ -16,21 +19,27 @@ class Person
     #accept a `last_name` parameter
     #search the `people` class attribute for instances with the same `last_name`
     #return a collection of matching instances
+    @@people.select{ |person| person.last_name == last_name }.map { |person|  person.to_s}
   end
 
   #have a `to_s` method to return a formatted string of the person's name
   def to_s
-    #return a formatted string as `first_name(space)last_name`
+    return "#{self.first_name} #{self.last_name}"
+  end
+
+  def self.people
+  	return @@people
   end
 end
 
 p1 = Person.new("John", "Smith")
 p2 = Person.new("John", "Doe")
-p3 = Person.new("Jane", "Smith")
 p4 = Person.new("Cool", "Dude")
+p3 = Person.new("Jane", "Smith")
+
+#Person.people.each { |person| puts "#{person.first_name} #{person.last_name}" }
 
 puts Person.search("Smith")
-
 # Should print out
 # => John Smith
 # => Jane Smith
